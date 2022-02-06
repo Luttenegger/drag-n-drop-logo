ae73f7531f40d8bf90d74b41bdcedada8c7f595b
Luttenegger <Luttenegger.Eric@gmail.com>
2022-02-06 12:06:05 -0700

    Adding a bit a styles for the drag enter and leave events

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -17,28 +17,44 @@ function createLogoHTML() {
         <img draggable="true" class="logo__circle green" src="img/ia-logo-dot-green.png"/>
         <img draggable="true" class="logo__circle black" src="img/ia-logo-dot-black.png"/>
         <img draggable="true" class="logo__circle black" src="img/ia-logo-dot-black.png"/>
-        <div class="logo__image">
-            <img class="logo__image__image" src="img/ia-logo-back.png"/>
-            <div class="logo__image__area"></div>
-            <div class="logo__image__area"></div>
-            <div class="logo__image__area"></div>
-            <div class="logo__image__area"></div>
-            <div class="logo__image__area"></div>
+        <div class="logo__container">
+            <img class="logo__container__image" src="img/ia-logo-back.png"/>
+            <div class="logo__container__zones">
+                <div class="logo__container__zones__zone"></div>
+                <div class="logo__container__zones__zone"></div>
+                <div class="logo__container__zones__zone"></div>
+                <div class="logo__container__zones__zone"></div>
+                <div class="logo__container__zones__zone"></div>
+            </div>
         </div>
     `;
 };
 
 function addImageDragEvents() {
     const circles = document.querySelectorAll('.logo__circle');
-    const areas = document.querySelectorAll('.logo__image__area');
+    const dropZones = document.querySelectorAll('.logo__container__zones__zone');
+    addCircleEvents(circles);
+    addDropZoneEvents(dropZones, circles);
+}
+
+function addCircleEvents(circles) {
     circles.forEach(circle => {
         circle.addEventListener('dragstart', function(event) {
-            console.log('Dragging');
+            event.target.style.opacity = .5;
+        })
+        circle.addEventListener('dragend', function(event) {
+            event.target.style.opacity = 1;
         })
     });
-    areas.forEach(area => {
-        area.addEventListener('dragover', function(event) {
-            console.log('Dragged over', event);
+}
+
+function addDropZoneEvents(dropZones, circles) {
+    dropZones.forEach((zone, i) => {
+        zone.addEventListener('dragover', function(event) {
+            event.target.style.opacity = 1;
+        });
+        zone.addEventListener('dragleave', function(event) {
+            event.target.style.opacity = 0;
         }) 
     })
 }
